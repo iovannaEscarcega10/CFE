b0731aec5b7f5b31431e2a159811fc8295559921
iovannaEscarceega10 <iovassoto@gmail.com>
2019-05-06 12:59:23 -0500

    mi primer commit

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,12 @@ import router from './router'
 import store from './store'
 import './registerServiceWorker'
 
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+import BootstrapVue from 'bootstrap-vue'
+
+Vue.use(BootstrapVue)
 Vue.config.productionTip = false
 
 new Vue({
@@ -11,3 +17,5 @@ new Vue({
   store,
   render: h => h(App)
 }).$mount('#app')
+
+
